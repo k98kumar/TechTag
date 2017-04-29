d675b4f8618d9be2d2fc0d77bb08d7181ea09e02
k98kumar <kash@wirelessprv-10-194-70-118.near.illinois.edu>
2017-04-29 18:36:00 -0500

    Sign in works. Hardware button to tag setup. Need to start ListPlayers.java

--- app/src/main/java/com/example/kash/techtag/CreateJoin.java
+++ app/src/main/java/com/example/kash/techtag/CreateJoin.java
@@ -1,24 +1,36 @@
 package com.example.kash.techtag;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.Color;
-import android.support.constraint.ConstraintLayout;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
 public class CreateJoin extends BaseActivity implements View.OnClickListener {
 
     String createOrJoin = "";
-    String code;
+    String groupName;
 
     String currentUserEmail;
 
     boolean isCreate = false;
 
-    EditText codeInput;
+    EditText nameInput;
+    TextView mStatusOutput;
+
+    private static final String GROUPS = "groups";
+
+    boolean inUse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,18 +38,20 @@ public class CreateJoin extends BaseActivity implements View.OnClickListener {
         setContentView(R.layout.activity_create_join);
 
         // Get the email of the user from previous activity
-        currentUserEmail = getIntent().getStringExtra("EMAIL");
+        // currentUserEmail = getIntent().getStringExtra("EMAIL");
+        currentUserEmail = FirebaseAuth.getInstance().getCurrentUser().getEmail();
 
         final Button selectButton = (Button) findViewById(R.id.selectButtonCreateJoin);
         final TextView createView = (TextView) findViewById(R.id.createLabel);
         final TextView joinView = (TextView) findViewById(R.id.joinLabel);
-        codeInput = (EditText) findViewById(R.id.createJoinInput);
+        mStatusOutput = (TextView) findViewById(R.id.statusOutputCreateJoin);
+        nameInput = (EditText) findViewById(R.id.createJoinInput);
 
-        codeInput.setFocusable(false);
-        codeInput.setFocusable(true);
-        codeInput.setFocusableInTouchMode(true);
-        codeInput.setHintTextColor(Color.parseColor("#999999"));
-        codeInput.setTextColor(Color.parseColor("#FFFFFF"));
+        mStatusOutput.setText("");
+
+        nameInput.setFocusable(false);
+        nameInput.setFocusable(true);
+        nameInput.setFocusableInTouchMode(true);
 
         createView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,10 +60,9 @@ public class CreateJoin extends BaseActivity implements View.OnClickListener {
                 createView.setTextColor(Color.parseColor("#FFFFFF"));
                 joinView.setBackground(getDrawable(R.color.white));
                 joinView.setTextColor(Color.parseColor("#000000"));
-                codeInput.setFocusable(false);
-                codeInput.setFocusable(true);
-                codeInput.setFocusableInTouchMode(true);
-                codeInput.setTextColor(Color.parseColor("#000000"));
+                nameInput.setFocusable(false);
+                nameInput.setFocusable(true);
+                nameInput.setFocusableInTouchMode(true);
                 createOrJoin = "create";
             }
         });
@@ -61,46 +74,106 @@ public class CreateJoin extends BaseActivity implements View.OnClickListener {
                 joinView.setTextColor(Color.parseColor("#FFFFFF"));
                 createView.setBackground(getDrawable(R.color.white));
                 createView.setTextColor(Color.parseColor("#000000"));
-                codeInput.setFocusable(false);
-                codeInput.setFocusable(true);
-                codeInput.setFocusableInTouchMode(true);
-                codeInput.setTextColor(Color.parseColor("#FFFFFF"));
+                nameInput.setFocusable(false);
+                nameInput.setFocusable(true);
+                nameInput.setFocusableInTouchMode(true);
                 createOrJoin = "join";
             }
         });
 
     }
 
-    /**
-     * Create a helper function that returns a boolean
-     * indicating whether the database contains the
-     * code of the group trying to be created or joined
-     * @return
-     */
-    public boolean inDatabase(String possibleCode) {
-        
+    @Override
+    public void onBackPressed() {
     }
 
     @Override
     public void onClick(View v) {
-        // Intent mIntent = new Intent(CreateJoin.this, ListPlayers.class);
+        Intent listPlayersIntent = new Intent(CreateJoin.this, ListPlayers.class);
         int i = v.getId();
+        if (nameInput.getText().toString().equals("")) {
+            mStatusOutput.setText(R.string.code_field_empty);
+            mStatusOutput.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mStatusOutput.setText("");
+                }
+            }, 4000);
+            return;
+        }
         if (i == R.id.selectButtonCreateJoin) {
+            String enteredCode = nameInput.getText().toString();
             switch (createOrJoin) {
-                case "create" :
-                    Intent mCreateIntent = new Intent(CreateJoin.this, ListPlayers.class);
-                    mCreateIntent.putExtra("CREATE_OR_JOIN", isCreate);
-                    break;
-                case "join" :
-                    if (codeInput.toString().equals("")) { // Rewrite what it equals
-                        Intent mJoinIntent = new Intent(CreateJoin.this, ListPlayers.class);
-                        startActivity(mJoinIntent);
+                case "create":
+                    if (codeInUse(mDatabase, enteredCode)) {
+                        mStatusOutput.setText(R.string.code_in_use);
+                        mStatusOutput.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                mStatusOutput.setText("");
+                            }
+                        }, 4000);
+                    } else {
+                        mDatabase.child(GROUPS).child(enteredCode).push().setValue(currentUserEmail);
+                        listPlayersIntent.putExtra("GROUP_CODE", enteredCode);
+                        CreateJoin.this.startActivity(listPlayersIntent);
                     }
                     break;
-                default :
+                case "join":
+                    if (codeInUse(mDatabase, enteredCode)) {
+                        mDatabase.child(GROUPS).child(enteredCode).push().setValue(currentUserEmail);
+                        listPlayersIntent.putExtra("GROUP_CODE", enteredCode);
+                        CreateJoin.this.startActivity(listPlayersIntent);
+                    } else {
+                        mStatusOutput.setText(R.string.code_not_in_use);
+                        mStatusOutput.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                mStatusOutput.setText("");
+                            }
+                        }, 4000);
+                    }
+                    break;
+                default:
+                    mStatusOutput.setText(R.string.option_not_selected);
+                    mStatusOutput.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            mStatusOutput.setText("");
+                        }
+                    }, 4000);
                     break;
             }
         }
+        if (i == R.id.button2) {
+            Intent tempIntent = new Intent(CreateJoin.this, MapLocations.class);
+            Log.d("Intent", "received");
+            startActivity(tempIntent);
+        }
     }
 
+    /**
+     * Helper function that returns a boolean indicating whether the
+     * database contains the code of the group trying to created or joined.
+     * @param dataRef       DatabaseReference argument
+     * @param codeToCheck   Name of group user tries to create or join
+     * @return  boolean stating if database already contains group name
+     */
+    public boolean codeInUse(DatabaseReference dataRef, final String codeToCheck) {
+        Log.d("valueEventListener", "Entered");
+        inUse = false;
+        dataRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (!dataSnapshot.child(GROUPS).hasChild(codeToCheck)) {
+                    inUse = true;
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+        return inUse;
+    }
 }
--- app/src/main/java/com/example/kash/techtag/MapLocations.java
+++ app/src/main/java/com/example/kash/techtag/MapLocations.java
@@ -1,24 +1,41 @@
 package com.example.kash.techtag;
 
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.lang.reflect.Array;
+import java.security.Key;
+import java.util.ArrayList;
 
 //public class MapLocations extends FragmentActivity implements OnMapReadyCallback {
 public class MapLocations extends BaseActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
+    long volumeUpPress = -1;
+    long volumeDownPress = -1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_locations);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -39,9 +56,81 @@ public class MapLocations extends BaseActivity implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        // To set multiple markers (simulating when a person is moving)
+        /*
+        MarkerOptions tempTest = new MarkerOptions().icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)).title("Marker in Sydney");
+
+        int tempTestLatitude = -34;
+        int[] tempTestLongitude = {140,141,142,143,144,145,146,147,148,149,150,151,152,153,154,155,156,157,158,159,160};
+
+        for (int i = 0; i < tempTestLongitude.length; i++) {
+            tempTest.position(new LatLng(tempTestLatitude, tempTestLongitude[i]));
+        }
+        */
+
+        ArrayList<Marker> mapMarkers = new ArrayList<>();
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        Marker sydneyMarker = mMap.addMarker(new MarkerOptions()
+                .position(sydney)
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+                .title("Sydney"));
+
+        // mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        LatLng moline = new LatLng(41.5067, -90.5151);
+        Marker molineMarker = mMap.addMarker(new MarkerOptions()
+                .position(moline)
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
+                .title("Moline"));
+
+        mapMarkers.add(sydneyMarker);
+        mapMarkers.add(molineMarker);
+
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+        for (Marker marker : mapMarkers) {
+            builder.include(marker.getPosition());
+        }
+        LatLngBounds bounds = builder.build();
+        int padding = 200; // offset from edges of the map in pixels
+        CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, padding);
+        mMap.moveCamera(cu);
+
     }
+
+    /**
+     * Player uses the volume up and down buttons to tag another
+     * player by pressing the volume up button and then the
+     * volume down button within 750 milliseconds.
+     * If volumeUpPress has not been clicked or the difference
+     * between volumeUpPress or volumeDownPress, the two
+     * variables get reset to their original values of -1.
+     * @param keyCode   The keycode of the button being pressed
+     * @param event     The event that the button is undergoing
+     * @return  True when one of the keys listed.
+     *          The return of the original function that is
+     *          being overridden.
+     */
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_VOLUME_UP:
+                volumeUpPress = System.currentTimeMillis();
+                Log.d("onKeyDown", "found " + volumeUpPress);
+                return true;
+            case KeyEvent.KEYCODE_VOLUME_DOWN:
+                volumeDownPress = System.currentTimeMillis();
+                if ((volumeUpPress != -1) && (volumeDownPress - volumeUpPress <= 750)) {
+                    // --------------------------- //
+                    // Tag the other person method //
+                    // --------------------------- //
+                } else {
+                    volumeUpPress = -1;
+                    volumeDownPress = -1;
+                }
+                return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
 }
